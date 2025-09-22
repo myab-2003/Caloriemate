988fd9a6306fcecfcf49007da1f29a3df1d2e7d9
myab-2003 <mahmoud.ashraf.ma740@gmail.com>
2025-09-22 21:37:26 +0300

    a

--- lib/Core/Utils/widgets/text.dart
+++ lib/Core/Utils/widgets/text.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomText extends StatelessWidget {
-  const CustomText({super.key , required this.title, this.style});
+  const CustomText({super.key , required this.title, this.style  });
     final String title;
     final TextStyle? style;
+   
   @override
   Widget build(BuildContext context) {
     return Text(
       title,
-      style: style,
+      style: style, 
     );
   }
 }
